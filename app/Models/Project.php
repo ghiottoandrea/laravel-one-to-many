88a83140d1b6228feb54364196f55f6d97aa5d2c
ghiottoandrea <andreaghiotto9@gmail.com>
2024-05-27 16:35:24 +0200

    Change type and project models for one-to-many relation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the Project
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
